1948c8b1351a95acffeba00f5a240bdaf1ac6d62
jasonberylmini <jasonberylmini@gmail.com>
2025-03-18 11:44:26 +0530

    coded pong game

---
+++ main.py
@@ -0,0 +1,46 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor('black')
+screen.setup(width = 800, height = 600)
+screen.title('The Arcade Game!')
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(l_paddle.move_down, "s")
+screen.onkey(l_paddle.move_up, "w")
+
+game_is_on =True
+while game_is_on:
+    time.sleep(ball.movement)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #Detect right paddle miss
+    if ball.xcor() > 420:
+        ball.reset_position()
+        score.l_point()
+
+    # Detect left paddle miss
+    if ball.xcor() < -420:
+        ball.reset_position()
+        score.r_point()
+
+screen.exitonclick()
